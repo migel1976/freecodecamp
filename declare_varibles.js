bf8bee99802223d748c22c830fdd9ee4a06aa1e5
smirnov <smirnov@alfasatcom.ru>
2023-10-19 19:08:05 +0300

    declare_varibles

---
+++ declare_varibles.js
@@ -0,0 +1,9 @@
+//DESCRIPTION
+//We tell JavaScript to create or declare a variable by putting the keyword var in front of it, like so:
+//
+// Use the var keyword to create a variable called myName
+
+// SOLUTION
+//
+//
+var myName
